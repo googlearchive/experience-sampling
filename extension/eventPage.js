6bfa674f72ba2055e6c3f9567278f4571fde7391
Christopher Thompson <cthomp@chromium.org>
2014-08-18 15:07:58 -0700

    Start adding more to install-time code for extension.
    Add .gitignore for *.pyc files.

--- extension/eventPage.js
+++ extension/eventPage.js
@@ -1,48 +1,60 @@
 /**
- * @fileoverview Description of this file.
+ * @fileoverview Event Page to track the various extension lifecycle and
+ * experience sampling events we are monitoring.
  */
-
-
-var getSurveysInterval = 10.0; // 10 minutes
-var submitResponsesInterval = 5.0; // 5 minutes
 
 /*
  * SETUP
- * Note that this fires:
+ * Note that this event fires:
  *  - When extension is first installed
  *  - When extension is updated
  *  - When Chrome is update
- * So we need to log that a user has registered to avoid prompting them multiple
- * times :-)
+ * We need to (if user is not "registered"):
+ *  - Show the user the notice & consent form
+ *  - If they consent, show the demographics form. If they complete the
+ *  demographics form, mark them as "registered".
+ *  - If they don't consent, trigger an uninstall when they hit "Submit" or
+ *  "Cancel".
  */
 chrome.runtime.onInstalled.addListener(function() {
-  console.log("Installed.");
+  // Load our registration status from LocalStorage.
+  //
+  var isRegistered = false;
+  chrome.storage.local.get("isRegistered", function (items) { isRegistered = items["isRegistered"] });
+  
+  if isRegistered {
+    return;
+  }
   
   // Setup LocalStorage for survey responses
   
   // Show on-install participant registration form
-  var opt = {body: 'Please take a survey about the screen you just saw',
-             icon: 'icon.png',
-             tag: 'test'};
-             
-  var notification = new window.Notification('New survey available', opt);
-  notification.onshow = function() { setTimeout(notification.close, 15000); };
-  //notification.onclick = function() { loadRegistration(); };
-  //notification.show();
+});
+
+// Listen for browser event to trigger popup
+chrome.experienceSamplingPrivate.onDecision.addListener(function(element, decision) {
+  console.log("Experience Sampling Decision");
+  console.log(element);
+  console.log(decision);
   
   // Get browser version and variations
   console.log("Chrome version: " + navigator.userAgent);
   chrome.experienceSamplingPrivate.getBrowserInfo(function(info) {
     console.log("Chrome trials: " + info.variations);
   });
-
+  
 });
 
-// Listen for browser event to trigger popup
-chrome.experienceSamplingPrivate.onDecision.addListener(function(element, decision) {
-  console.log("Experience Sampling Decision");
-  console.log("----------------");
-  console.log(element);
-  console.log(decision);
-  console.log("----------------");
-});
+function showSurveyPrompt() {
+  var opt = {body: 'Click here to take a survey about the screen you just saw',
+             icon: 'icon.png',
+             tag: 'test'};
+             
+  var notification = new window.Notification('New survey available', opt);
+  notification.onshow = function() { setTimeout(notification.close, 15000); };
+  notification.onclick = function() { loadSurvey(); };
+}
+
+function loadSurvey() {
+  window.open(
+}
