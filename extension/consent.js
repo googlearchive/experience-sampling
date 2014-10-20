d1705bd242d888eb53d9acd8dcce01d14ee67311
APF <felt@google.com>
2014-10-19 21:30:13 -0700

    Don't set a timeout, just uninstall

--- extension/consent.js
+++ extension/consent.js
@@ -67,7 +67,7 @@ function userRejectConsent(event) {
   console.log('Consent rejected');
   setConsentStorageValue(constants.CONSENT_REJECTED);
   $('consent-form-holder').classList.add('hidden');
-  setTimeout(chrome.management.uninstallSelf);
+  chrome.management.uninstallSelf;
 }
 
 /**
