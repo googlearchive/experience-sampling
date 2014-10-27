74a209f55be7a873f43331bbc925c8d2a91f37b4
APF <felt@google.com>
2014-10-27 15:42:57 +1100

    Only require a single checkbox to be checked

--- extension/surveys/survey-generator.js
+++ extension/surveys/survey-generator.js
@@ -57,6 +57,13 @@ function FixedQuestion(
 FixedQuestion.prototype = Object.create(Question.prototype);
 FixedQuestion.prototype.constructor = FixedQuestion;
 
+function checkRequiredForm(questionName) {
+  var elems = document.getElementsByName(questionName);
+  for (var i = 0; i < elems.length; i++) {
+    elems[i].removeAttribute('required');
+  }
+}
+
 /**
  * Creates the DOM representation of a FixedQuestion question.
  * @return {object} The DOM node that contains the question.
@@ -82,7 +89,21 @@ FixedQuestion.prototype.makeDOMTree = function() {
         input.setAttribute('name', shrunkenQuestion);
         input.setAttribute('type', this.questionType);
         input.setAttribute('value', shrunkenAnswer);
-        input.setAttribute('required', this.required);
+        if (this.required) {
+          input.setAttribute('required', this.required);
+          if (this.questionType === constants.QuestionType.CHECKBOX) {
+            // By default, HTML5 requires *all* of the checkboxes to be checked
+            // for submission. Since we only want one of a group to be submitted
+            // submitted, remove the requirement once one is checked.
+            input.addEventListener('click', function removeRequired(unused) {
+              var elems = document.getElementsByName(shrunkenQuestion);
+              for (var i = 0; i < elems.length; i++) {
+                elems[i].removeAttribute('required');
+                elems[i].removeEventListener('click', removeRequired);
+              }
+            });
+          }
+        }
         answer.appendChild(input);
 
         var label = document.createElement('label');
