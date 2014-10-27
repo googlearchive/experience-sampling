e7488bb8065c45b4865e30584104728a26209b7e
Chris Thompson <cthomp@chromium.org>
2014-10-27 15:11:40 -0700

    Forgot to commit the cleaned up background.js

--- extension/background.js
+++ extension/background.js
@@ -8,14 +8,15 @@
  * demographics) before they can begin to answer real survey questions.
  */
 
-var cesp = {};  // namespace variable
+
+/**
+ * cesp namespace.
+ */
+var cesp = cesp || {};
 
 cesp.operatingSystem = "";
 
 // Settings.
-cesp.SERVER_URL = 'https://chrome-experience-sampling.appspot.com';
-cesp.SUBMIT_SURVEY_ACTION = '/_ah/api/cesp/v1/submitsurvey';
-cesp.XHR_TIMEOUT = 4000;
 cesp.NOTIFICATION_TITLE = 'New Chrome survey available!';
 cesp.NOTIFICATION_BODY = 'Your feedback makes Chrome better.';
 cesp.NOTIFICATION_BUTTON = 'Take survey!';
@@ -28,9 +29,6 @@ cesp.SURVEY_COUNT_RESET_ALARM_NAME = 'surveyCountReset';
 cesp.NOTIFICATION_ALARM_NAME = 'notificationTimeout';
 cesp.UNINSTALL_ALARM_NAME = 'uninstallAlarm';
 cesp.READY_FOR_SURVEYS = 'readyForSurveys';
-cesp.DB_NAME = 'pendingResponsesDB';
-cesp.DB_VERSION = 1;
-cesp.QUEUE_ALARM_NAME = 'surveySubmissionAlarm';
 
 // SETUP
 
@@ -295,236 +293,3 @@ function loadSurvey(element, decision, timePromptShown, timePromptClicked) {
 // Trigger the new survey prompt when the participant makes a decision about an
 // experience sampling element.
 chrome.experienceSamplingPrivate.onDecision.addListener(showSurveyNotification);
-
-/**
- * A question and response.
- * @constructor
- * @param {string} question The question being answered.
- * @param {string} answer The answer to that question.
- */
-function Response(question, answer) {
-  this.question = question;
-  this.answer = answer;
-}
-
-/**
- * A completed survey.
- * @constructor
- * @param {string} type The type of survey.
- * @param {int} participantId The participant ID.
- * @param {Date} dateTaken The date and time when the survey was taken.
- * @param {Array.Response} responses An array of Response objects.
-*/
-function SurveyRecord(type, participantId, dateTaken, responses) {
-  this.type = type;
-  this.participantId = participantId;
-  this.dateTaken = dateTaken;
-  this.responses = responses;
-}
-
-/**
- * A completed survey pending submission to the backend.
- * @constructor
- * @param {SurveyRecord} surveyRecord The completed survey that is pending.
- * @param {int} timeToSend The time when we want the survey to be sent, in ms
- *     since epoch. The survey will not be sent before this time, but may be 
- *     delayed arbitrarily.
- * @param {int} tries The number of attempts made to send this survey so far.
- */
-function PendingSurveyRecord(surveyRecord, timeToSend, tries) {
-  this.surveyRecord = surveyRecord;
-  this.timeToSend = timeToSend;
-  this.tries = tries;
-}
-
-/**
- * Saves a completed survey into the database of pending completed surveys.
- * Applies an exponential backoff based on the number of attempts made to
- * submit the survey so far.
- * @param {SurveyRecord} surveyRecord The completed survey to add to the
- *     queue.
- * @param {int=} tries The number of tries so far (optional, defaults to 0).
- */
-function saveSurveyRecord(surveyRecord, tries) {
-  if (!tries)
-    var tries = 0;
-
-  withObjectStore('PendingSurveyRecords', 'readwrite', function(store) {
-    var timeToSend = Date.now() + sendingDelay(tries);
-    var pendingSurveyRecord = new PendingSurveyRecord(surveyRecord,
-        timeToSend, tries);
-    var request = store.add(pendingSurveyRecord);
-  });
-}
-
-/**
- * Compute the sending delay, in ms. This is an exponential backoff.
- * @param {int} tries The number of tries to send so far.
- * @returns {int} The delay in ms.
- */
-function sendingDelay(tries) {
-  return (Math.pow(2, tries) - 1) * 60000;
-}
-
-/**
- * Get all pending surveyRecords with timeToSend less than the current time,
- * and try to send them. If sending succeeds, delete them from the database. If
- * sending fails, update the timeToSend so we try again later.
- * @params {Alarm} alarm The alarm that triggered.
- */
-function processQueue(alarm) {
-  if (alarm.name != cesp.QUEUE_ALARM_NAME) return;
-
-  function makeSuccessCallback(id) {
-    return function(response) {
-      deleteSurvey(id);
-    };
-  }
-
-  function makeErrorCallback(id) {
-    return function(status) {
-      updateTimeToSend(id);
-    };
-  }
-
-  withObjectStore('PendingSurveyRecords', 'readonly', function(store) {
-    var surveysToSubmit = [];
-
-    var index = store.index('timeToSend');
-    var keyRange = IDBKeyRange.upperBound(Date.now());
-    index.openCursor(keyRange).onsuccess = function(event) {
-      var cursor = event.target.result;
-      if (cursor) {
-        surveysToSubmit.push({id: cursor.value.id,
-            surveyRecord: cursor.value.surveyRecord});
-        cursor.continue();
-      } else {
-        // After collecting all the surveys over the cursor, make async calls
-        // to sendSurvey.
-        for (var i = 0; i < surveysToSubmit.length; i++) {
-          var id = surveysToSubmit[i].id;
-          var surveyRecord = surveysToSubmit[i].survey;
-          sendSurvey(surveyRecord,
-            makeSuccessCallback(id),
-            makeErrorCallback(id));
-        }
-      }
-    };
-  });
-}
-chrome.alarms.onAlarm.addListener(processQueue);
-
-/**
- * Delete the survey with the specified key from the database.
- * @param {int} id The ID primary key of survey to delete.
- */
-function deleteSurveyRecord(id) {
-  withObjectStore('PendingSurveyRecords', 'readwrite', function(store) {
-    var request = store.delete(id);
-  });
-}
-
-/**
- * Updates the timeToSend field of a survey with a given ID key based on the
- * number of times we've tried to send it.
- * @param {int} id The ID primary key of the survey to update.
- */
-function updateTimeToSend(id) {
-  withObjectStore('PendingSurveyRecords', 'readwrite', function(store) {
-    var request = store.get(id);
-    request.onsuccess =  function(event) {
-      var record = event.target.result;
-      record.tries = record.tries + 1;
-      record.timeToSend = Date.now() + sendingDelay(record.tries);
-      var request = store.put(record);
-    }
-  });
-}
-
-/**
- * Perform a callback action after opening the database and a given
- * object store.
- * @param {string} storeName The name of the object store to open.
- * @param {string} mode The transaction mode ('readwrite' or 'readonly').
- * @param {function(IDBObjectStore)} action 
- */ 
-function withObjectStore(storeName, mode, action) {
-  var request = indexedDB.open(cesp.DB_NAME, cesp.DB_VERSION);
-  request.onsuccess = function(event) {
-    var db = event.target.result;
-    var transaction = db.transaction([storeName], mode);
-    var objectStore = transaction.objectStore(storeName);
-    action(objectStore);
-  };
-  request.onerror = function(event) {
-    console.log("Database Error: " + event.target.errorCode);
-  }
-  request.onupgradeneeded = setupPendingResponsesDatabase;
-}
-
-/**
- * Sets up our object store and index for our database.
- * Used for the 'onupgradeneeded' event listener.
- * @param {event} event The event this listener is receiving.
- */
-function setupPendingResponsesDatabase(event) {
-  var db = event.target.result;
-  var objectStore = db.createObjectStore(
-      'PendingSurveyRecords', { keyPath: 'id', autoIncrement: true});
-  objectStore.createIndex('timeToSend', 'timeToSend', {unique: false});
-}
-
-/**
- * Sends a completed survey to the CESP backend via XHR.
- * @param {SurveyRecord} surveyRecord The completed survey to send to the
- *     backend.
- * @param {function(string)} successCallback A function to call on receiving a
- *     successful response (HTTP 204). It should look like
- *     "function(response) {...};" where "response" is the text of the response
- *     (if there is any).
- * @param {function(!number=)} errorCallback A function to call on receiving an
- *     error from the server, or on timing out. It should look like
- *     "function(status) {...};" where "status" is an HTTP status code integer,
- *     if there is one. For a timeout, there is no status.
- */
-function sendSurveyRecord(surveyRecord, successCallback, errorCallback) {
-  var url = cesp.SERVER_URL + cesp.SUBMIT_SURVEY_ACTION;
-  var method = 'POST';
-  var dateTaken = surveyRecord.dateTaken.toISOString();
-  // Get rid of timezone 'Z' on end of ISO String for AppEngine compatibility.
-  if (dateTaken.slice(-1) === 'Z') {
-    dateTaken = dateTaken.slice(0, -1);
-  }
-  var data = {
-    'date_taken': dateTaken,
-    'participant_id': surveyRecord.participantId,
-    'responses': [],
-    'survey_type': surveyRecord.type
-  };
-  for (var i = 0; i < surveyRecord.responses.length; i++) {
-    data.responses.push(surveyRecord.responses[i]);
-  }
-  var xhr = new XMLHttpRequest();
-  function onLoadHandler(event) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 204) {
-        successCallback(xhr.response);
-      } else {
-        errorCallback(xhr.status);
-      }
-    }
-  }
-  function onErrorHandler(event) {
-    errorCallback(xhr.status);
-  }
-  function onTimeoutHandler(event) {
-    errorCallback();
-  }
-  xhr.open(method, url, true);
-  xhr.setRequestHeader('Content-Type', 'application/JSON');
-  xhr.timeout = cesp.XHR_TIMEOUT;
-  xhr.onload = onLoadHandler;
-  xhr.onerror = onErrorHandler;
-  xhr.ontimeout = onTimeoutHandler;
-  xhr.send(JSON.stringify(data));
-}
