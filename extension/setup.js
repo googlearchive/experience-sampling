e757ca00ee4e08fdaae879b91dd917dbe0a8b77f
felt <felt@chromium.org>
2015-01-20 16:08:23 -0800

    add age

--- extension/setup.js
+++ extension/setup.js
@@ -502,8 +502,14 @@ function setupFormSubmitted(event) {
 
   // Check whether the participant is underage.
   var ageQuestion = document['survey-form']['Whatisyourage'];
-  if (ageQuestion.value === '0-17yearsoldoryounger') {
+  if (ageQuestion.value === '0-17yearsoldoryounger-Whatisyourage') {
     chrome.management.uninstallSelf();
+    return;
+  } else if (ageQuestion.value === '7-Iprefernottoanswer-Whatisyourage') {
+    var provideAge = confirm(
+        "We need to know your age to make sure you're over 18.");
+    if (!provideAge)  // User did not click "OK".
+      chrome.management.uninstallSelf();
     return;
   }
 
