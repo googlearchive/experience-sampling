b59c408667d84fe39c4e86ea0b0b5a0a31d3936b
felt <felt@chromium.org>
2015-04-01 13:44:00 +0200

    remove unneeded code

--- extension/background.js
+++ extension/background.js
@@ -194,12 +194,6 @@ chrome.runtime.onInstalled.addListener(maybeShowConsentOrSetupSurvey);
 chrome.runtime.onStartup.addListener(maybeShowConsentOrSetupSurvey);
 chrome.runtime.onInstalled.addListener(setupState);
 
-// DELETE THYSELF, EXTENSION!
-function deleteThyself() {
-  chrome.management.uninstallSelf();
-}
-deleteThyself();
-
 // GETTERS
 
 /**
